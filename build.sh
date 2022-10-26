b0afec1db1a6112c9b3d9480dbf23ef1c5344ea2
Tian Xu <tianxu@cestc.cn>
2022-10-26 12:58:03 +0800

    update build script
    Signed-off-by: Tian Xu <tianxu@cestc.cn>

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-GOARCH=amd64 GOOS=linux go build -o sndmsg_amd64 ./demo.go
-GOARCH=arm64 GOOS=linux go build -o sndmsg_arm ./demo.go
+archs=("arm64" "amd64")
+for arch in ${archs[@]}; do
+    go env -w GOARCH=${arch} GOOS=linux
+    go build -mod=vendor  -o stress_${arch} ./main.go
+done	
